a62258a02535ccbee0c7bebff4fdf41233e1d810
MD. Saimun Rahman <saimunmail@gmail.com>
2025-05-12 10:22:40 +0600

    login form changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#272727] to-[#000000] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <LoginForm />
+        <div className="w-full max-w-sm md:max-w-3xl">
+          <LoginForm />
+        </div>
       </div>
     </main>
   );
