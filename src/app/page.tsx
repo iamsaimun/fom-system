78d069b2f6ab0b166f7b0caf4a6d44d6dff07ebd
MD. Saimun Rahman <saimunmail@gmail.com>
2025-05-12 10:09:27 +0600

    login form ui implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import { LoginForm } from "~/components/auth/login-form";
+
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#272727] to-[#000000] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <div>
-          <h1>LOGIN PANEL</h1>
-        </div>
+        <LoginForm />
       </div>
     </main>
   );
